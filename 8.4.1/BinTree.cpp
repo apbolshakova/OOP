c28192e7e2058d413219248c79dcb0046195e8dd
julody <julody@mail.ru>
2019-11-13 00:43:06 +0300

    Изменена реализация BinTree в соответствии с требованиями задачи

--- 8.4.1/BinTree.cpp
+++ 8.4.1/BinTree.cpp
@@ -3,43 +3,84 @@
 #include <exception>
 #include <conio.h>
 
-enum Side {UNKNOWN, LEFT, RIGHT};
+class InorderTreeNode;
+class PreorderTreeNode;
+class PostorderTreeNode;
 
 class BinTreeNode
 {
-	BinTreeNode* left;
-	BinTreeNode* right;
 public:
 	int val;
-	BinTreeNode(int val = 0) : val(val) { left = nullptr; right = nullptr; }
-	~BinTreeNode();
-	BinTreeNode* getLeftChild() { return left; }
-	BinTreeNode* getRightChild() { return right; }
-	void addNode(int i, Side side);
+	BinTreeNode* left;
+	BinTreeNode* right;
+	BinTreeNode(int val = 0) : val(val)
+	{
+		left = nullptr;
+		right = nullptr;
+	}
+	~BinTreeNode()
+	{
+		if (left) delete left;
+		if (right) delete right;
+	}
+	void addNode(int val, BinTreeNode *x);
 };
 
-void BinTreeNode::addNode(int val, Side side = UNKNOWN)
+void BinTreeNode::addNode(int val, BinTreeNode *x = nullptr)
 {
-	if (side == LEFT) BinTreeNode::left = new BinTreeNode(val);
-	if (side == RIGHT) BinTreeNode::right = new BinTreeNode(val);
+	if (!x) x = this;
+	if (val < x->val)
+	{
+		if (x->left) addNode(val, x->left);
+		else x->left = new BinTreeNode(val);
+	}
+	else
+	{
+		if (x->right) addNode(val, x->right);
+		else x->right = new BinTreeNode(val);
+	}
 }
 
-BinTreeNode::~BinTreeNode()
-{
-	if (BinTreeNode::left) delete BinTreeNode::left;
-	if (BinTreeNode::right) delete BinTreeNode::right;
-}
+void traverse(InorderTreeNode *node, int& count, int& val);
+void traverse(PostorderTreeNode *node, int& count, int& val);
+void traverse(PreorderTreeNode *node, int& count, int& val);
 
 class InorderTreeNode : public BinTreeNode
 {
 public:
-	InorderTreeNode(int val) : BinTreeNode(val) {}
+	InorderTreeNode(int val) : BinTreeNode(val) {};
+	int &operator[](int count)
+	{
+		count++;
+		InorderTreeNode* root = this;
+		int val = -1;
+		traverse(root, count, val);
+		return val;
+	}
 };
+
+void traverse(InorderTreeNode *node, int& count, int& val)
+{
+	InorderTreeNode* left = static_cast<InorderTreeNode *>(node->left);
+	InorderTreeNode* right = static_cast<InorderTreeNode *>(node->right);
+	
+	if (left) traverse(left, count, val);
+	if (!count) return;
+
+	count--;
+	if (!count)
+	{
+		val = node->val;
+		return;
+	}
+
+	if (right) traverse(right, count, val);
+}
 
 std::ostream& operator<< (std::ostream &ostr, InorderTreeNode* node)
 {
-	InorderTreeNode* left = static_cast<InorderTreeNode *>(node->getLeftChild());
-	InorderTreeNode* right = static_cast<InorderTreeNode *>(node->getRightChild());
+	InorderTreeNode* left = static_cast<InorderTreeNode *>(node->left);
+	InorderTreeNode* right = static_cast<InorderTreeNode *>(node->right);
 	if (left) ostr << left;
 	ostr << " " << node->val;
 	if (right) ostr << right;
@@ -50,12 +91,35 @@ class PostorderTreeNode : public BinTreeNode
 {
 public:
 	PostorderTreeNode(int val) : BinTreeNode(val) {}
+	int &operator[](int count)
+	{
+		count++;
+		PostorderTreeNode* root = this;
+		int val = -1;
+		traverse(root, count, val);
+		return val;
+	}
 };
+
+void traverse(PostorderTreeNode *node, int& count, int& val)
+{
+	PostorderTreeNode* left = static_cast<PostorderTreeNode *>(node->left);
+	PostorderTreeNode* right = static_cast<PostorderTreeNode *>(node->right);
+
+	if (left) traverse(left, count, val);
+	if (!count) return;
+
+	if (right) traverse(right, count, val);
+	if (!count) return;
+
+	count--;
+	if (!count) val = node->val;
+}
 
 std::ostream& operator<< (std::ostream &ostr, PostorderTreeNode* node)
 {
-	PostorderTreeNode* left = static_cast<PostorderTreeNode *>(node->getLeftChild());
-	PostorderTreeNode* right = static_cast<PostorderTreeNode *>(node->getRightChild());
+	PostorderTreeNode* left = static_cast<PostorderTreeNode *>(node->left);
+	PostorderTreeNode* right = static_cast<PostorderTreeNode *>(node->right);
 	if (left) ostr << left;
 	if (right) ostr << right;
 	return ostr << " " << node->val;
@@ -65,36 +129,78 @@ class PreorderTreeNode : public BinTreeNode
 {
 public:
 	PreorderTreeNode(int val) : BinTreeNode(val) {}
+	int &operator[](int count)
+	{
+		count++;
+		PreorderTreeNode* root = this;
+		int val = -1;
+		traverse(root, count, val);
+		return val;
+	}
 };
+
+void traverse(PreorderTreeNode *node, int& count, int& val)
+{
+	PostorderTreeNode* left = static_cast<PostorderTreeNode *>(node->left);
+	PostorderTreeNode* right = static_cast<PostorderTreeNode *>(node->right);
+
+	count--;
+	if (!count) val = node->val;
+	
+	if (left) traverse(left, count, val);
+	if (!count) return;
+
+	if (right) traverse(right, count, val);
+}
 
 std::ostream& operator<< (std::ostream &ostr, PreorderTreeNode* node)
 {
-	PreorderTreeNode* left = static_cast<PreorderTreeNode *>(node->getLeftChild());
-	PreorderTreeNode* right = static_cast<PreorderTreeNode *>(node->getRightChild());
+	PreorderTreeNode* left = static_cast<PreorderTreeNode *>(node->left);
+	PreorderTreeNode* right = static_cast<PreorderTreeNode *>(node->right);
 	ostr << " " << node->val;
 	if (left) ostr << left;
 	if (right) ostr << right;
 	return ostr;
 }
 
+void removeHandledVals(std::string &input)
+{
+	if (input.find(" ") != std::string::npos) input.erase(0, input.find(" ") + 1);
+	else input.clear();
+}
+
 int main()
 {
-	BinTreeNode *root = new BinTreeNode(1);
-	root->addNode(2, LEFT);
-	root->addNode(3, RIGHT);
-	BinTreeNode *last = root->getLeftChild();
-	last->addNode(4, LEFT);
-	last->addNode(5, RIGHT);
-	last = last->getRightChild();
-	last->addNode(7, LEFT);
-	last = root->getRightChild();
-	last->addNode(6, RIGHT);
+	std::string input = "";
+	std::getline(std::cin, input, '\n');
+	int val;
+	try { val = std::stoi(input); }
+	catch (...)
+	{
+		std::cout << "Invalid input";
+		_getch();
+		return 0;
+
+	}
+	BinTreeNode* root = new BinTreeNode(val);
+	removeHandledVals(input);
+	while (!input.empty())
+	{
+		try { val = std::stoi(input); }
+		catch (...) { break; }
+		val = std::stoi(input);
+		root->addNode(val);
+		removeHandledVals(input);
+	}
 
 	std::cout << "Inorder:" << static_cast<InorderTreeNode *>(root) << std::endl;
 	std::cout << "Postorder:" << static_cast<PostorderTreeNode *>(root) << std::endl;
 	std::cout << "Preorder:" << static_cast<PreorderTreeNode *>(root) << std::endl;
 
-	delete root;
+	std::cout << "Second from inorder: " << (*static_cast<InorderTreeNode *>(root))[1] << std::endl;
+	std::cout << "Second from postorder: " << (*static_cast<PostorderTreeNode *>(root))[1] << std::endl;
+	std::cout << "Second from preorder: " << (*static_cast<PreorderTreeNode *>(root))[1] << std::endl;
+
 	_getch();
 	return 0;
 }
